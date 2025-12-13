ec50d48075353d823a521d663b04ab8b9421ad7e
Stanislav Nemchenko <stas.buka@gmail.com>
2025-12-14 00:56:26 +0200

    Add featured tools block and enhance layout
    - Implemented FeaturedToolsBlock component to display popular tools.
    - Updated layout and styles for better responsiveness and user experience.
    - Added new CSS modules for FeaturedToolsBlock and ToolCard components.
    - Modified global styles and layout configuration for improved design.
    - Updated package dependencies for modern-normalize and lucide-react.
    - Removed unused API and type files to clean up the codebase.

--- store/auth.store.ts
+++ store/auth.store.ts
@@ -1,47 +1,28 @@
-"use client";
-
-import axios from "axios";
 import { create } from "zustand";
 
 interface User {
-  id: string;
-  email: string;
+    id: string;
+    email: string;
 }
 
 interface AuthState {
-  user: User | null;
-  loading: boolean;
-  setUser: (user: User | null) => void;
-  logout: () => void;
-  fetchUser: () => Promise<void>;
+    user: User | null;
+    isAuthenticated: boolean;
+    setUser: (user: User | null) => void;
+    logout: () => void;
 }
 
-const API_URL = process.env.NEXT_SERVER_URL;
+export const useAuthStore = create<AuthState>((set) => ({
+    user: null,
+    isAuthenticated: false,
 
-export const useAuth = create<AuthState>((set) => ({
-  user: null,
-  loading: true,
+    setUser: (user) => set({ user, isAuthenticated: !!user }),
 
-  setUser: (user) => set({ user }),
-
-  logout: () => {
-    set({ user: null });
-    if (typeof window !== "undefined") {
-      document.cookie =
-        "accessToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;";
-    }
-  },
-
-  fetchUser: async () => {
-    set({ loading: true });
-    try {
-      const res = await axios.get(`${API_URL}/me`, {
-        withCredentials: true, // чтобы отправлять cookie
-      });
-      set({ user: res.data, loading: false });
-    } catch (err) {
-      console.error("Fetch user failed:", err);
-      set({ user: null, loading: false });
-    }
-  },
+    logout: () => {
+        set({ user: null, isAuthenticated: false });
+        if (typeof window !== "undefined") {
+            document.cookie =
+                "accessToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;";
+        }
+    },
 }));
