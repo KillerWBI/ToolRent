49fe923d0d1ce4b3065c1f9dcb7c8f3b901d4b26
GetAoP <108660418+KillerWBI@users.noreply.github.com>
2025-12-15 22:11:25 +0100

    Refactor layout to include ProtectedRoute component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,33 @@
 // app/layout.tsx
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
+import Footer from "@/components/layout/Footer/Footer";
+import Header from "@/components/layout/Header/Header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import ProtectedRoute from "../components/shared/ProtectedRoute";
 import "./globals.css";
 
 const inter = Inter({
-  weight: ["400", "500", "600", "700"], // Regular, Medium, SemiBold, Bold
-  subsets: ["latin", "cyrillic"],
-  display: "swap",
-  variable: "--font-Inter",
+    weight: ["400", "500", "600", "700"], // Regular, Medium, SemiBold, Bold
+    subsets: ["latin", "cyrillic"],
+    display: "swap",
+    variable: "--font-Inter",
 });
 
 export const metadata: Metadata = {
-  title: {
-    template: "%s | ToolNext",
-    default: "ToolNext — Оренда професійних інструментів",
-  },
-  description:
-    "Зручний сервіс для оренди та здачі будівельних інструментів. Широкий вибір категорій, чесні рейтинги та відгуки. Знайдіть потрібний інструмент або заробляйте на власному обладнанні.",
-  openGraph: {
-    title: "ToolNext — Оренда професійних інструментів",
+    title: {
+        template: "%s | ToolNext",
+        default: "ToolNext — Оренда професійних інструментів",
+    },
     description:
-      "Платформа для оренди інструментів. Дрилі, пили, генератори та інше обладнання для вашого ремонту та будівництва.",
-    siteName: "ToolNext",
-    locale: "uk_UA",
-    type: "website",
-  },
+        "Зручний сервіс для оренди та здачі будівельних інструментів. Широкий вибір категорій, чесні рейтинги та відгуки. Знайдіть потрібний інструмент або заробляйте на власному обладнанні.",
+    openGraph: {
+        title: "ToolNext — Оренда професійних інструментів",
+        description:
+            "Платформа для оренди інструментів. Дрилі, пили, генератори та інше обладнання для вашого ремонту та будівництва.",
+        siteName: "ToolNext",
+        locale: "uk_UA",
+        type: "website",
+    },
 };
 export default function RootLayout({
   children,
@@ -38,10 +39,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable}`}>
-        <Header />
-        <main>{children}</main>
-        {modal}
-        <Footer />
+        <ProtectedRoute>
+          <Header/>
+            <main>{children}</main>
+            {modal}
+          <Footer/>
+        </ProtectedRoute>
       </body>
     </html>
   );
