269b057d542bd239ea10a4e47faedb71e6af23a4
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-12-15 23:13:47 +0200

    benefits- block / styles for all devices

--- components/home/BenefitsBlock/BenefitsBlock.tsx
+++ components/home/BenefitsBlock/BenefitsBlock.tsx
@@ -2,7 +2,9 @@ import css from "./BenefitsBlock.module.css";
 
 export default function BenefitsBlock(){
     return (
-        <section className={css.benefitsBlock}>
+        
+        <section className={css.benefitsBlock} >
+             <div className="container">
             <div className={css.benefitsDesc}>
         <h2 className={css.benefitsTitle}>ToolNext — платформа для швидкої та зручної оренди інструментів</h2>
         <p className = {css.benefitsText}>ToolNext допомагає знайти потрібний інструмент у декілька кліків.
@@ -12,26 +14,28 @@ export default function BenefitsBlock(){
         <ul className = {css.benefitsLst}>
                 <li className = {css.benefitsListItem}>
                     <svg className ={css.benefitsSvg} width="48" height = "48">
-                        <use href="../../../public/svg/"/>
+                        <use href="/svg/sprite.svg#service-toolbox"/>
                     </svg>
-                 <h3>Легкий доступ до інструментів</h3>
+                 <h3 className={css.benefitsListTitel}>Легкий доступ до інструментів</h3>
                 <p className={css.benefitsListText}>Знаходьте потрібний інструмент у своєму районі без зайвих дзвінків і пошуків. Просто введіть назву — і отримайте варіанти поруч із вами.</p>
                 </li>
         <li className = {css.benefitsListItem}>
                     <svg className ={css.benefitsSvg} width="48" height = "48">
-                        <use href="../../../public/svg/"/>
+                        <use href="/svg/sprite.svg#checkbook"/>
                     </svg>
-                 <h3>Швидке бронювання</h3>
+                 <h3 className={css.benefitsListTitel}>Швидке бронювання</h3>
                 <p className={css.benefitsListText}>Бронюйте інструменти в кілька кліків. Жодних складних форм чи довгих очікувань — тільки простий та зручний процес.</p>
                 </li >
                     <li className = {css.benefitsListItem}>
                     <svg className ={css.benefitsSvg} width="48" height = "48">
-                        <use href="../../../public/svg/"/>
+                        <use href="/svg/sprite.svg#manage-accounts"/>
                     </svg>
-                 <h3>Зручне управління</h3>
+                 <h3 className={css.benefitsListTitel}>Зручне управління</h3>
                 <p className={css.benefitsListText}>Додавайте свої інструменти в каталог, редагуйте оголошення та контролюйте оренду. ToolNext допомагає перетворити зайві інструменти на додатковий дохід.</p>
                 </li>
-        </ul >
-        </section>        
+                </ul >
+                </div> 
+            </section>
+           
     );
 }
