853c13bdf102ee6fbe8e8a3cfc7ac67574cf7376
abrosenkov <abrosenkov91@gmail.com>
2025-12-16 18:08:14 +0100

    change info block

--- components/tools/ToolGallery/ToolGallery.tsx
+++ components/tools/ToolGallery/ToolGallery.tsx
@@ -2,11 +2,13 @@ import css from "./ToolGallery.module.css";
 
 export const ToolGallery = () => {
   return (
-    <img
-      className={css.image}
-      src={
-        "https://ftp.goit.study/img/tools-next/692db3ffab59e437964311d4.webp"
-      }
-    />
+    <div className={css.imgWrap}>
+      <img
+        className={css.image}
+        src={
+          "https://ftp.goit.study/img/tools-next/692db3ffab59e437964311d4.webp"
+        }
+      />
+    </div>
   );
 };
--- components/tools/ToolInfoBlock/ToolInfoBlock.tsx
+++ components/tools/ToolInfoBlock/ToolInfoBlock.tsx
@@ -5,7 +5,15 @@ export const ToolInfoBlock = () => {
     <div className={css.wrapper}>
       <h2 className={css.title}>Дриль алмазного свердління</h2>
       <p className={css.price}>900 грн/день</p>
-      <div className={css.userProfile}></div>
+      <div className={css.userProfile}>
+        <div className={css.userIcon}></div>
+        <div className={css.userInfo}>
+          <p className={css.userName}>Антон Петренко</p>
+          <button className={css.profileBtn} type="button">
+            Переглянути профіль
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
