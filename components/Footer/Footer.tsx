520ed1b91ef30281ae5737f56adfa423b66217fc
Yaremchuk-Sergey <bb01082014@gmail.com>
2025-12-23 11:00:39 +0200

    Прив’язав рік у футері до поточної дати в календарі

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ import { useAuthStore } from "@/store/auth.store";
 
 export default function Footer() {
   const { isAuthenticated } = useAuthStore();
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer className={styles.footer}>
@@ -75,7 +76,9 @@ export default function Footer() {
         </div>
 
         <div className={styles.line}></div>
-        <div className={styles.copy}>© 2025 ToolNext. Всі права захищені.</div>
+        <div className={styles.copy}>
+          © {currentYear} ToolNext. Всі права захищені.
+        </div>
       </div>
     </footer>
   );
